439a759ad51ad482fed299e248dadc09003d7160
gulledrengen <gustav.werk@gmail.com>
2024-11-26 11:00:15 +0100

    added tileset and shitty player movement

--- P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/PlayerMovement.cs
+++ P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] Transform trans;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] BoxCollider2D coll;
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    private void OnCollisionStay2D(Collision2D collider) {
-        Debug.Log(collider.gameObject.name);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
+        // gets move input
+        UnityEngine.Vector2 inputDir = new UnityEngine.Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Mathf.Abs(inputDir.x) == 1 || Mathf.Abs(inputDir.y) == 1)
+        {
+            // THIS SHOULD BE CHANGED!!!
+            if (!Input.anyKeyDown)
+            {
+                return;
+            }
 
-    private void Move()
-    {
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            trans.position = new Vector2(trans.position.x + 1, trans.position.y);
-        } else if (Input.GetKeyDown(KeyCode.A))
-        {
-            trans.position = new Vector2(trans.position.x - 1, trans.position.y);
+            // checks if desired move is passable 
+            if (Physics2D.OverlapCircle(rb.position + inputDir, 0.1f))
+            {
+                Debug.Log("cant move to here");
+                return;
+            }
+
+            Move(inputDir);
         }
     }
+
+    private void Move(UnityEngine.Vector2 inputDir)
+    {
+            rb.position += inputDir;
+    }
+
+    /*IEnumerator MoveGradually(UnityEngine.Vector2 targetPos)
+    {
+        trans.position = new UnityEngine.Vector2((targetPos.x - trans.position.x) *0.1f, (targetPos.y - trans.position.y) *0.1f);
+    }*/
 }
