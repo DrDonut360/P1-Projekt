15723481d26105df32ed11fa66ff2e77d5b8190f
gulledrengen <gustav.werk@gmail.com>
2024-11-26 11:50:41 +0100

    improved movement controller, by adding gradual movement

--- P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/PlayerMovement.cs
+++ P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.Collections;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -10,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] BoxCollider2D coll;
     // Start is called before the first frame update
+
+    private bool canMove = true;
     void Start()
     {
     }
@@ -21,26 +25,45 @@ public class PlayerMovement : MonoBehaviour
         UnityEngine.Vector2 inputDir = new UnityEngine.Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (Mathf.Abs(inputDir.x) == 1 || Mathf.Abs(inputDir.y) == 1)
         {
-            // THIS SHOULD BE CHANGED!!!
-            if (!Input.anyKeyDown)
+            // prevents diagonal movement
+            if (Mathf.Abs(inputDir.x) == Mathf.Abs(inputDir.y))
             {
                 return;
             }
 
-            // checks if desired move is passable 
+            if (!canMove)
+            {
+                return;
+            }
+
+            // checks if desired move is blocked
             if (Physics2D.OverlapCircle(rb.position + inputDir, 0.1f))
             {
                 Debug.Log("cant move to here");
                 return;
             }
 
-            Move(inputDir);
+            //Move(inputDir);
+            StartCoroutine(MoveGradually(inputDir));
         }
     }
 
-    private void Move(UnityEngine.Vector2 inputDir)
+    private void Move(UnityEngine.Vector2 moveDir)
     {
-            rb.position += inputDir;
+            rb.position += moveDir;
+    }
+
+    IEnumerator MoveGradually(UnityEngine.Vector2 moveDir) 
+    {
+        canMove = false;
+
+        UnityEngine.Vector2 targetPos = rb.position + moveDir;
+        while (rb.position != targetPos)
+        {
+            rb.position += moveDir * 0.1f;
+            yield return new WaitForSeconds(.01f);
+        }
+        canMove = true;
     }
 
     /*IEnumerator MoveGradually(UnityEngine.Vector2 targetPos)
