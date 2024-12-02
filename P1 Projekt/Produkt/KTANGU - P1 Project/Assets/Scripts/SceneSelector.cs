85c49fe2e8cdd1ef27747e4e838f5fce9500aaa1
paragoncode <magnus.laursen00@gmail.com>
2024-12-02 13:20:01 +0100

    Revert "some changes"
    This reverts commit e4b76925b7454c84d2cac3fda304c1dfdc9b29c2.

--- P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/SceneSelector.cs
+++ P1 Projekt/Produkt/KTANGU - P1 Project/Assets/Scripts/SceneSelector.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements.Experimental;
 
 public class SceneSelector : MonoBehaviour
 {
-    
-    public Animator transition;
-    public float TransAnimTime = 1f;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+
+    //}
 
     //hav et dictionary med scener, led efter scener. tjeck hvilken scene vi er p�.
     //Tjekker om playeren g�r ind i vores m�l, hvis den g�r dette vll den g� til en ny scene.
@@ -19,26 +26,10 @@ public class SceneSelector : MonoBehaviour
         Scene currentScene = SceneManager.GetActiveScene();
 
         string sceneName = currentScene.name;
-
-        //int currentLvlIndex = SceneManager.GetActiveScene().buildIndex + 1;
         Debug.Log($"the name of the scene is : {sceneName}");
 
 
         if (other.gameObject.CompareTag("Player") && sceneName == "Start-End")
-        {
-            Debug.Log("Loading 1st level");
-            SceneManager.LoadScene("LevelOne");
-
-        }
-        else if (other.gameObject.CompareTag("Player"))
-        {
-            LoadNextLevel();
-        }
-        
-
-        
-
-        /*if (other.gameObject.CompareTag("Player") && sceneName == "Start-End")
         {
             Debug.Log("Loading 1st level");
             SceneManager.LoadScene("LevelOne");
@@ -58,26 +49,10 @@ public class SceneSelector : MonoBehaviour
         {
             Debug.Log("Loading Main Menu");
             SceneManager.LoadScene("MainMenu");
-        }*/
-    }
-    public void LoadNextLevel ()
-    {
-        Debug.Log($"Loading {SceneManager.GetActiveScene().name}");
-
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
-    IEnumerator LoadLevel (int LevelIndex)
-    {
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(TransAnimTime);
-
-        SceneManager.LoadScene(LevelIndex);
-    }
-
+    
 
 
 }
